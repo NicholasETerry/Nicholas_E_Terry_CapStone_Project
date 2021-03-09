84c4fe73f2cc324be98e6906497d23432ca79ea9
NicholasETerry <nicholas.e.terry@gmail.com>
2021-03-09 13:35:10 -0600

    added tests to testLibrary

--- Nicholas_E_Terry_CapStone/Models/Test.cs
+++ Nicholas_E_Terry_CapStone/Models/Test.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,17 @@ namespace Nicholas_E_Terry_CapStone.Models
 {
     public class Test
     {
+        [Key]
+        public int Id { get; set; }
+        public int TestNumber { get; set; }
+        public string TestQuestion { get; set; }
+        public string IsCorrect { get; set; }
+        public string IsWrongOne { get; set; }
+        public string IsWrongTwo { get; set; }
+        public string IsWrongThree { get; set; }
+
+        [ForeignKey("UserModel")]
+        public int Test_UserModel_Id { get; set; }
+        public UserModel Test_UserModel { get; set; }
     }
 }
--- Nicholas_E_Terry_CapStone/Models/TestLibrary.cs
+++ Nicholas_E_Terry_CapStone/Models/TestLibrary.cs
@@ -17,6 +17,42 @@ namespace Nicholas_E_Terry_CapStone.Models
         {
             List<Test> newTestList = new List<Test>();
 
+            Test newTestOne = new Test
+            {
+                TestNumber = 1,
+                TestQuestion = "Correct the portion in Bold: The first time the United States imported more oil than it exported, Americans should have" +
+                               " realized that an energy crisis was imminent and could happen in the future.",
+                IsCorrect = "Will be imminent and happening soon.",
+                IsWrongOne = "Was imminent and could happen in the future.",
+                IsWrongTwo = "Could happen imminently in the future.",
+                IsWrongThree = "Was imminent."
+            };
+            newTestList.Add(newTestOne);
+
+            Test newTestTwo = new Test
+            {
+                TestNumber = 2,
+                TestQuestion = "Correct the portion in bold: The dramatic monologue was one poetic form congenial to Rober Browning;" +
+                               " it let him explore a character's mind without the simplifications demanded by stage productions.",
+                IsCorrect = "Browning, letting him explore",
+                IsWrongOne = "Browning; it let him explore",
+                IsWrongTwo = "Browning, which let him explore",
+                IsWrongThree = "Browning, that lets him explore"
+            };
+            newTestList.Add(newTestTwo);
+
+            Test newTestThree = new Test
+            {
+                TestNumber = 3,
+                TestQuestion = "Correct the portion in bold: At the Constitutional Convention of 1787, the proposal to replace the existiong Articles of" +
+                               " Confederation with a federal constitution were met with fierce opposition.",
+                IsCorrect = "Were met with",
+                IsWrongOne = "Having been met with",
+                IsWrongTwo = "Met with",
+                IsWrongThree = "Met their"
+            };
+            newTestList.Add(newTestThree);
+
             return newTestList;
         }
 
